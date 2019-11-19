350f7946b0b1c60722d1c2c0ad9aabc6434341cc
Gabriel França <dev.gabrielfranca@gmail.com>
2019-11-19 00:04:34 -0300

    finish user screen

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
-
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
+  const [newUser, setNewUser] = useState('');
   const [users, setUsers] = useState([]);
+
+  const handleAddUser = async () => {
+    const response = await api.get(`users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    setUsers([...users, data]);
+    setNewUser('');
+
+    Keyboard.dismiss();
+  };
 
   return (
     <Container>
@@ -14,9 +32,13 @@ export default function Main() {
           autoCorrect={false}
           autoCapitalize="none"
           placeholder="Adicionar usuário"
+          value={newUser}
+          onChangeText={text => setNewUser(text)}
+          returnKeyType="send"
+          onSubmitEditing={handleAddUser}
         />
 
-        <SubmitButton>
+        <SubmitButton onPress={handleAddUser}>
           <Icon name="add" size={20} color="#fff" />
         </SubmitButton>
       </Form>
